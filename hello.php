f1ab48efb416b4272dda1ebcf44a1d747ca44bd2
pjotilju <pjotilju@greeny.cs.tlu.ee>
2016-09-06 15:15:54 +0300

    first php lines

---
+++ hello.php
@@ -0,0 +1,14 @@
+<?php
+	$firstName = "Pjotr";
+	$lastName = "Iljukov";
+	
+	// echo prints out variable
+	echo $firstName;
+	echo " ";
+	echo $lastName;
+	echo " ";
+	// punkt liidab stringe
+	echo $firstName." ".$lastName;
+
+
+?>
